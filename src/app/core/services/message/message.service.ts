9eac29b86cb09d0da1f661251d1f3c357cc8c858
tahgio <psthiago28@gmail.com>
2023-05-01 18:00:08 +0100

    little optimization of messages services

--- src/app/core/services/message/message.service.ts
+++ src/app/core/services/message/message.service.ts
@@ -12,28 +12,32 @@ const MESSAGE_ICONS = {
   providedIn: 'root'
 })
 export class MessageService {
-  private toastRef!: ComponentRef<ToastComponent>;
+  private toastRef!: ComponentRef<ToastComponent> | null;
 
-  constructor(
-    private appRef: ApplicationRef,
-) {
-
- }
-
-
+  constructor(private appRef: ApplicationRef) {}
 
   showToast(type: ToastTypes, message: string) {
+    if(this.toastRef) {
+      this.appRef.detachView(this.toastRef.hostView);
+      this.toastRef.destroy()
+    }
+    //Create element
     const toastElement = document.createElement('app-toast')
+    //Get its reference
     const toastRef = this.appRef.bootstrap(ToastComponent, toastElement) as ComponentRef<ToastComponent>;
+    //Inject inputs
     toastRef.instance.message = message
     toastRef.instance.type = type
     toastRef.instance.icon = MESSAGE_ICONS[type]
     toastRef.instance.animation = "animate-fadeIn"
+    //Save Ref
     this.toastRef = toastRef
     this.appRef.attachView(toastRef.hostView);
     const domElem = (toastRef.hostView as EmbeddedViewRef<any>)
-      .rootNodes[0] as HTMLElement;
+    .rootNodes[0] as HTMLElement;
+    //Append Child
     document.body.appendChild(domElem);
+    //Hide Toast 
     setTimeout(() => {
       this.hideToast()
     }, 3000);
@@ -43,10 +47,12 @@ export class MessageService {
     if (this.toastRef) {
       this.toastRef.instance.animation = "animate-fadeOut"
       setTimeout(() => {
-        this.appRef.detachView(this.toastRef.hostView);
-        this.toastRef.destroy();
+        if (this.toastRef) {
+          this.appRef.detachView(this.toastRef.hostView);
+          this.toastRef.destroy();
+        }
+        this.toastRef = null
       }, 500)
-      // document.body.removeChild(this.toastRef.location.nativeElement);
     }
   }
 
