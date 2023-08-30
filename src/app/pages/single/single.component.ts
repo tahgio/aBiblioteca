e639552096150cf96481521940d014bb6dcbeb37
Thiago Augusto Souza <psthiago28@gmail.com>
2023-08-30 13:17:39 +0100

    route created

--- src/app/pages/single/single.component.ts
+++ src/app/pages/single/single.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single',
   templateUrl: './single.component.html',
 })
-export class SingleComponent {}
+export class SingleComponent implements OnInit {
+  itemId!: string | null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Get itemId from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    this.itemId = routeParams.get('itemId');
+  }
+}
